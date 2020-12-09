b1caf388a173a27f563735fc74198227fbd9b9fe
Foysal6862 <61239971+Foysal6862@users.noreply.github.com>
2020-12-10 02:34:07 +0600

    this -> 01. implicit binding example-o2

--- this.js
+++ this.js
@@ -1,10 +1,34 @@
 // 01. Implicit binding
 
-var sakib = {
+//example:01
+/* var sakib = {
     name: "sakib",
     age: 30,
     printPlayerName: function() {
         console.log(this.name);
     }
 }
+sakib.printPlayerName(); */
+
+//example:02
+
+var printPlayerNameFunction = function(obj) {
+    obj.printPlayerName = function() {
+        console.log(this.name);
+        console.log(this.age);
+    };
+};
+
+var sakib = {
+    name :'Sakib',
+    age : 39,
+};
+var tamim = {
+    name :'Tamim',
+    age : 37,
+};
+
+printPlayerNameFunction(sakib);
+printPlayerNameFunction(tamim);
 sakib.printPlayerName();
+tamim.printPlayerName();

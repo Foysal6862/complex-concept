8e100343b485dc98890ede43eac00e84dce387a9
Foysal6862 <61239971+Foysal6862@users.noreply.github.com>
2020-12-11 17:16:41 +0600

    hoisting

---
+++ hoistings.js
@@ -0,0 +1,24 @@
+/* let LANGUAGE = 'Java';
+let language = 'Javascript';
+
+function getLanguage() {
+    if (!language) {
+        //let language;
+        //language = undefined;
+        //language = LANGUAGE;
+
+        let language = LANGUAGE;
+    }
+    return language;
+}
+console.log(`I love ${getLanguage()}`); */
+
+/* myFunc();
+function myFunc() {
+    console.log(`I love Javascript`);
+} */
+myFunc();
+const myFunc = function() {
+    let language = 'Javascript';
+    console.log(language);
+}
